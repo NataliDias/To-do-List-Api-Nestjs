94f81971b54be0c201e6517e67c0c2d510eb05c3
Natalí Dias <82180157+Natali-Dias-DevAprender@users.noreply.github.com>
2022-09-12 23:00:21 -0300

    Update app.module.ts

--- todolist/src/app.module.ts
+++ todolist/src/app.module.ts
@@ -7,16 +7,16 @@ import { TarefaModule } from './tarefa/modules/tarefa.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({//Vai ter o que é necessario pra se conectar com o DB
-      type: 'mysql',//Definindo objeto do tipo mysql
-      host: 'localhost', //Definindo que o host é o localhost
-      port: 3306, //Definindo a posta do local host (Porta padrão)
-      username: 'root', //Definindo o username do mysql
-      password: 'Juliano', //Senha padrão DB
-      database: 'db_todo', //Nome da tabela que criamos
-      entities: [Tarefa, Categoria], //Criando o TB tarefa
-      synchronize: true //Sincroniza todos os dados e alterações no DB
-      //Com essas informações é possivel se conectar com o BD
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost', 
+      port: 3306, 
+      username: 'root', 
+      password: 'Juliano', 
+      database: 'db_todo', 
+      entities: [Tarefa, Categoria],
+      synchronize: true 
+      
     }),
     TarefaModule,
     CategoriaModule
