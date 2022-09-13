e54cc04cea62ba2f6c42629be0cd5af6c38f5026
Natalí Dias <82180157+Natali-Dias-DevAprender@users.noreply.github.com>
2022-09-12 23:08:47 -0300

    Update tarefa.controller.ts

--- todolist/src/tarefa/controllers/tarefa.controller.ts
+++ todolist/src/tarefa/controllers/tarefa.controller.ts
@@ -4,25 +4,25 @@ import { Tarefa } from "../entities/tarefa.entity";
 import { TarefaService } from "../service/tarefa.service";
 
 
-@Controller('/tarefa')//Vai injetar no service e mapear a função get. /tarefa sera o caminho
+@Controller('/tarefa')
 export class TarefaController{
     constructor(
         private readonly service: TarefaService){}
 
     @Get()//Mapeando o Get no controller
-    @HttpCode(HttpStatus.OK)//Retorna um status se der certo o Get ele retorna Ok
+    @HttpCode(HttpStatus.OK)
     findAll(): Promise<Tarefa[]>{
-        return this.service.findAll()//Busca tudo dentro do banco
+        return this.service.findAll()
     }
 
-    @Get('/:id')//Passar o caminho de tarefa/id
+    @Get('/:id')
     @HttpCode(HttpStatus.OK)
-    findById(@Param('id', ParseIntPipe) id: number): Promise<Tarefa>{//Esperando um Id, que vai ser do tipo inteiro
+    findById(@Param('id', ParseIntPipe) id: number): Promise<Tarefa>{
         return this.service.findById(id)//Buscando um Id
     }
 
-    @Get('/nome/:nome')//tarefa/nome/l
-    @HttpCode(HttpStatus.OK)//Retorna um status se der certo o Get ele retorna Ok
+    @Get('/nome/:nome')
+    @HttpCode(HttpStatus.OK)
     findByNome(@Param('nome')nome: string): Promise<Tarefa[]>{
         return this.service.findByNome(nome)
     } 
